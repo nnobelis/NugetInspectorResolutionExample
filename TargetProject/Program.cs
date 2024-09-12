3f0cc5803bf1f4ce9d0de8079a4626ddea1a20c3
Nicolas Nobelis <nicolas.nobelis@bosch.com>
2024-09-12 16:14:07 +0200

    Initial commit

---
+++ TargetProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+using HealthChecks.CosmosDb;
+
+namespace TargetProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var x = new Class1();
+        }
+    }
+}
